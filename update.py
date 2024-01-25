7f2147bdbe86e20acf72662ad97d31db82e38f4d
lskyset <kenn.lab@hotmail.fr>
2024-01-26 03:33:42 +0900

    temp fix

--- update.py
+++ update.py
@@ -1,11 +1,15 @@
-import urllib.request
+import json
 import os
+import sqlite3
+import urllib.request
 
-host = 'prd-priconne-redive.akamaized.net'
-manifest_path = '/dl/Resources/%s/Jpn/AssetBundles/iOS/manifest/manifest_assetmanifest'
-masterdata_path = '/dl/Resources/%s/Jpn/AssetBundles/Windows/manifest/masterdata_assetmanifest'
-bundles_path = '/dl/pool/AssetBundles'
-default_ver = 10041300
+host = "prd-priconne-redive.akamaized.net"
+manifest_path = "/dl/Resources/%s/Jpn/AssetBundles/iOS/manifest/manifest_assetmanifest"
+masterdata_path = (
+    "/dl/Resources/%s/Jpn/AssetBundles/Windows/manifest/masterdata_assetmanifest"
+)
+bundles_path = "/dl/pool/AssetBundles"
+default_ver = 10053100
 max_test_amount = 30
 test_multiplier = 10
 
@@ -13,7 +17,7 @@ ver = default_ver
 i = 0
 while i < max_test_amount:
     guess = ver + i * test_multiplier
-    url = f'http://{host}{manifest_path % guess}'
+    url = f"http://{host}{manifest_path % guess}"
     try:
         response = urllib.request.urlopen(url)
         if response.code == 200:
@@ -23,17 +27,52 @@ while i < max_test_amount:
         pass
     i += 1
 
-urllib.request.urlretrieve(f'http://{host}{masterdata_path % ver}', 'masterdata')
+urllib.request.urlretrieve(f"http://{host}{masterdata_path % ver}", "masterdata")
 
-cdb_path = 'master.cdb'
-db_path = 'master.db'
-with open('masterdata') as fd:
-    hash_ = fd.readline().split(',')[1]
-    urllib.request.urlretrieve(f'http://{host}{bundles_path}/{hash_[:2]}/{hash_}', cdb_path)
+cdb_path = "master.cdb"
+db_path = "master.db"
+with open("masterdata") as fd:
+    hash_ = fd.readline().split(",")[1]
+    urllib.request.urlretrieve(
+        f"http://{host}{bundles_path}/{hash_[:2]}/{hash_}", cdb_path
+    )
 
-stream = os.popen(f'Coneshell_call.exe -cdb  {cdb_path} {db_path}')
+stream = os.popen(f"Coneshell_call.exe -cdb {cdb_path}")
 output = stream.read()
 print(output)
 
-os.remove('masterdata')
-os.remove('master.cdb')
+os.remove("masterdata")
+os.remove("master.cdb")
+
+conn = sqlite3.connect(db_path)
+
+
+def clean_db(conn: sqlite3.Connection, lookup: dict[str, dict]):
+    c = conn.cursor()
+
+    data = c.execute("select name from sqlite_master where type='table'").fetchall()
+    table_names = [t for (t, *_) in data if t.startswith("v1")]
+
+    whitelist = [
+        "clan_battle_2_map_data",
+        "clan_battle_period",
+        "wave_group_data",
+        "enemy_parameter",
+        "clan_battle_period",
+    ]
+
+    for table in table_names:
+        if not lookup[table]["name"] in whitelist:
+            continue
+
+        query = ""
+        for k, v in lookup[table]["cols"].items():
+            query += f"alter table {table} rename column '{k}' to '{v}';"
+        query += f"alter table {table} rename to {lookup[table]['name']};"
+        c.executescript(query)
+    conn.commit()
+
+
+with open("lookup_table.json", "r", encoding="utf-8") as fp:
+    lt = json.load(fp)
+clean_db(conn, lt)
