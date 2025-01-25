957694181783b72d07e3d907298b8ac414bc53f1
lskyset <kenn.lab@hotmail.fr>
2025-01-26 05:12:09 +0900

    hello

--- update.py
+++ update.py
@@ -10,7 +10,7 @@ masterdata_path = (
     "/dl/Resources/%s/Jpn/AssetBundles/Windows/manifest/masterdata_assetmanifest"
 )
 bundles_path = "/dl/pool/AssetBundles"
-default_ver = 10060800
+default_ver = 10061500
 max_test_amount = 30
 test_multiplier = 10
 
